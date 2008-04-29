b2fc9ec8a4de0a08684699976a0dc8e1dc98dcb3
quicke <quicke@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-04-29 21:02:58 +0000

    version 0.8
    git-svn-id: https://plugins.svn.wordpress.org/suggest-comments/tags/0.8@43473 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -4,21 +4,32 @@ Donate link: http://blog.quickes-wohnzimmer.de/
 Tags: comments, fun, survey
 Requires at least: 2.0.2
 Tested up to: 2.5
-Stable tag: 0.3
+Stable tag: 0.8
 
-Suggest some comments to your lazy visitors. Can also be used as a simple survey tool on the comment form. Inspired by <a href="http://www.vorspeisenplatte.de">Vorspeisenplatte</a> and <a href="http://riesenmaschine.de/">Riesenmaschine</a> 
+Suggest some comments to your lazy visitors. Can also be used as a simple survey tool on the comment form. 
 
 == Description ==
 
 This plugin is just for fun. Sometimes a visitor would like to say that he likes a post. Suggest him some standard phrases.
 
+It's still in developement. So check for new releases daily!
+
+New features in 0.8
+Enable/disable for posts and pages seperately
+Randomize comment list
+
+Fixed in 0.7
+quotes can be used now
+implemented support for "comment" and "comment-text" textarea id's
+activation might have caused problems with WP version < 2.3
+
 == Installation ==
 
 1. Upload the zip file to the `/wp-content/plugins/` directory
-1. Extract the file
-1. Activate the plugin through the 'Plugins' menu in WordPress
-1. Configure the phrases and a short introduction under 'Settings / Suggest Comments'
-1. The suggestions will automatically appear on the bottom of the comment form
+2. Extract the file
+3. Activate the plugin through the 'Plugins' menu in WordPress
+4. Configure the phrases and a short introduction under 'Settings / Suggest Comments'
+5. The suggestions will automatically appear on the bottom of the comment form
 
 == Frequently Asked Questions ==
 
--- suggestcomments.php
+++ suggestcomments.php
@@ -3,11 +3,11 @@
 Plugin Name: Suggest Comments
 Plugin URI: http://blog.quickes-wohnzimmer.de/suggestcomments
 Description: Suggest some comments to your lazy visitors :)  
-Version: 0.3
+Version: 0.8
 Author: quicke
 Author URI: http://blog.quickes-wohnzimmer.de
 
-  Copyright 2007  Rene Springborn  (email : plugins at quickes-wohnzimmer.de)
+  Copyright 2008  Rene Springborn  (email : plugins at quickes-wohnzimmer.de)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -24,7 +24,11 @@ Author URI: http://blog.quickes-wohnzimmer.de
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+if ($wp_version >= '2.3') {
+       require_once(ABSPATH.'wp-admin/includes/upgrade.php');
+} else {
+       require_once(ABSPATH.'wp-admin/upgrade-functions.php') ;
+}
 
 add_action('wp_print_scripts', 'suggestcomment_js_header' );
 add_action('comment_form','suggestcomment_form_items');
@@ -32,16 +36,30 @@ add_action('admin_menu', 'suggestcomment_add_options_to_admin');
 
 register_activation_hook(__FILE__,'suggestcomment_activate');
 
-//js for replacing the current comment with a suggested one
+/*additional javascript for comment form
+suggestcomment_replace_comment(comment)
+replacing the current comment with a suggested one
+*/
+
 function suggestcomment_js_header(){
+ $suggestcomment_prompt = get_option('suggestcomment_prompt');
+ $commentlist = suggestcomment_get_comments();
 ?>
 <script type="text/javascript">
 //<![CDATA[
-function suggestcomment_replace_comment(comment)
+function suggestcomment_replace_comment(commentid)
 {
-replaceIt = confirm("Current comment will be replaced.");
- if (replaceIt==true){ 
- 		window.document.forms['commentform'].elements['comment-text'].value=comment
+var commenttext = new Array ();
+<?php 
+foreach ($commentlist as $sugcomment){
+echo "commenttext[$sugcomment->comment_id]=\"$sugcomment->comment_text\";\n";
+}
+?>
+replaceIt = confirm("<?php echo $suggestcomment_prompt;?>");
+ if (replaceIt==true){
+    commentForm=window.document.forms['commentform'];
+		if (commentForm.elements['comment-text']){commentForm.elements['comment-text'].value=commenttext[commentid]};
+		if (commentForm.elements['comment']){commentForm.elements['comment'].value=commenttext[commentid]};
 		} 
 }
 //]]>
@@ -60,16 +78,41 @@ add_options_page('suggestcomment', 'Suggest Comments', '8', basename(__FILE__), 
 function suggestcomment_options_subpanel() {
 		// database version
 		$suggestcomment_db_version = get_option('suggestcomment_db_version');
+		// prompt
+		$suggestcomment_prompt = get_option('suggestcomment_prompt');
 		//short introduction above the given comments 
 		$suggestcomment_describe = stripslashes(get_option('suggestcomment_describe'));
-
+		//post types which given comments are shown on
+		$suggestcomment_posttypes = stripslashes(get_option('suggestcomment_posttypes'));
+		//show random comments
+		$suggestcomment_randomize = stripslashes(get_option('suggestcomment_randomize'));
+		//how many random comments?
+		$suggestcomment_randomize_count = stripslashes(get_option('suggestcomment_randomize_count'));
+						
+		
 		//update field information in database
 		if (isset($_POST['submit'])) {
-			 global $wpdb;
- 	 			$new_comment = $_POST['suggestcomment_new_comment'];
+			  global $wpdb;
+ 	 			$commentlist = suggestcomment_get_comments();
+				 foreach ($commentlist as $sugcomment){
+					suggestcomment_update_comment($sugcomment->comment_id,$_POST["suggestcomment_".$sugcomment->comment_id]);
+				 }				
+				$new_comment = $_POST['suggestcomment_new_comment'];
 				if ($new_comment<>""){suggestcomment_add_comment($new_comment);}	
 				$suggestcomment_describe = stripslashes($_POST['suggestcomment_describe']);
 				update_option('suggestcomment_describe', $wpdb->escape($suggestcomment_describe));
+				$suggestcomment_prompt = stripslashes($_POST['suggestcomment_prompt']);
+				update_option('suggestcomment_prompt', $wpdb->escape($suggestcomment_prompt));
+				$suggestcomment_posttypes_posts = stripslashes($_POST['suggestcomment_posttypes_posts']);
+				update_option('suggestcomment_posttypes_posts', $wpdb->escape($suggestcomment_posttypes_posts));
+				$suggestcomment_posttypes_pages = stripslashes($_POST['suggestcomment_posttypes_pages']);
+				update_option('suggestcomment_posttypes_pages', $wpdb->escape($suggestcomment_posttypes_pages));
+				$suggestcomment_randomize = stripslashes($_POST['suggestcomment_randomize']);
+				update_option('suggestcomment_randomize', $wpdb->escape($suggestcomment_randomize));
+				$suggestcomment_randomize_count = stripslashes($_POST['suggestcomment_randomize_count']);
+				//simple validation (and correction) of randomize count value - number, at least 2 and at most number of comments
+				if (!is_numeric($suggestcomment_randomize_count)|$suggestcomment_randomize_count<2|$suggestcomment_randomize_count>count($commentlist)){$suggestcomment_randomize_count=count($commentlist);}
+				update_option('suggestcomment_randomize_count', $wpdb->escape($suggestcomment_randomize_count));
 				}
 		
 		// is there a comment to be deleted?
@@ -104,18 +147,62 @@ function suggestcomment_options_subpanel() {
 			foreach ($commentlist as $sugcomment){
 				echo "<tr id='".$sugcomment->comment_id."' class='alternate author-self status-future'>\n";
 				echo "<td><input size=\"80\"type=\"text\" name='suggestcomment_".$sugcomment->comment_id."' value=\"".htmlspecialchars(stripslashes($sugcomment->comment_text))."\"></td>\n";
-				echo "<td><div style=\"text-align: center\"><a href='".GetBackLink()."&delete=".$sugcomment->comment_id."'>L&ouml;schen</a></div></td>\n";
+				echo "<td><div style=\"text-align: center\"><a href='".GetBackLink()."&delete=".$sugcomment->comment_id."'>Delete</a></div></td>\n";
 				echo "</tr>\n";
 			}  
 
 ?>
 			<tr class='alternate author-self status-future'><td><input size="80" type= "text" name="suggestcomment_new_comment"></td><td><div style="text-align: center">(new entry)</div></td></tr>
-			<tr class='alternate author-self status-future'><td><input size="80" type= "text" name="suggestcomment_describe" value="<?php echo $suggestcomment_describe; ?>"></td><td><div style="text-align: center">(show above selection)</div></td></tr>
-					</tbody>
-				</table>
-
+			</tbody>
 			</table>
-			 </fieldset>
+			
+			<table class="widefat">
+			<thead>
+				<tr>
+					<th scope="col">Description</th>
+					<th scope="col">&nbsp;</th>
+				</tr>
+			</thead>
+	
+			<tr class='alternate author-self status-future'><td><input size="80" type= "text" name="suggestcomment_prompt" value="<?php echo $suggestcomment_prompt; ?>"></td><td><div style="text-align: center">(prompt before replacing)</div></td></tr>
+			<tr class='alternate author-self status-future'><td><input size="80" type= "text" name="suggestcomment_describe" value="<?php echo $suggestcomment_describe; ?>"></td><td><div style="text-align: center">(show above selection)</div></td></tr>
+				</tbody>
+				</table>
+			
+			
+			<table class="widefat">
+			<thead>
+				<tr>
+					<th scope="col">Show suggested comments on</th>
+				</tr>
+			</thead>
+			
+			<tr class='alternate author-self status-future'><td>
+			<input type="checkbox" name="suggestcomment_posttypes_posts" value="1" <?php if (get_option('suggestcomment_posttypes_posts')=='1') echo "checked='checked'"; ?>> Posts<br>
+			</td></tr>
+			<tr class='alternate author-self status-future'><td>
+			<input type="checkbox" name="suggestcomment_posttypes_pages" value="1" <?php if (get_option('suggestcomment_posttypes_pages')=='1') echo "checked='checked'"; ?>> Pages<br>
+			</td></tr>
+			</tbody>
+			</table>
+			
+			<table class="widefat">
+			<thead>
+				<tr>
+					<th scope="col">Randomize comments</th>
+				</tr>
+			</thead>
+			
+			<tr class='alternate author-self status-future'><td>
+			<input type="checkbox" name="suggestcomment_randomize" value="1" <?php if (get_option('suggestcomment_randomize')=='1') echo "checked='checked'"; ?>> Show random suggestions<br>
+			</td></tr>
+			<tr class='alternate author-self status-future'><td>
+			<input type="text" size="2" name="suggestcomment_randomize_count" value="<?php echo get_option('suggestcomment_randomize_count'); ?>"> How many comments should be displayed? (at least 2)<br>
+			</td></tr>
+			</tbody>
+			</table>
+			 			
+		 </fieldset>
 		 	
       		<div class="submit">
 			<input type="submit" name="submit" value="<?php _e('Update Options') ?> &raquo;" />
@@ -134,16 +221,29 @@ function GetBackLink() {
 		return $_SERVER['PHP_SELF'] . "?page=" .  $page;	
 	}
 	
-
 //display selection on comment form
 function suggestcomment_form_items()
 {
+if((get_option('suggestcomment_posttypes_posts')=='1' && get_post_type()=='post')|(get_option('suggestcomment_posttypes_pages')=='1' && get_post_type()=='page')){
 $commentlist = suggestcomment_get_comments();
 $suggestcomment_describe = stripslashes(get_option('suggestcomment_describe'));
+echo $suggestcomment_describe."\n<ul>\n";
 
-echo $suggestcomment_describe."<br>";
+if (get_option('suggestcomment_randomize')=='1')
+{
+//random comments 
+$sugcomment_keys = array_rand($commentlist,get_option('suggestcomment_randomize_count'));
+foreach ($sugcomment_keys as $sugcommentnum){
+ $sugcomment = $commentlist[$sugcommentnum];
+ echo "<li><a href=\"#respond\" onclick=\"suggestcomment_replace_comment('".$sugcomment->comment_id."')\">".stripslashes($sugcomment->comment_text)."</a></li>\n";
+}
+}else{
+//all comments
 foreach ($commentlist as $sugcomment){
- echo "<input type=\"radio\" name=\"suggestcomment_comment\" value=\"".stripslashes($sugcomment->comment_text)."\" onchange=\"suggestcomment_replace_comment(this.value)\"> ".stripslashes($sugcomment->comment_text."<br>");
+ echo "<li><a href=\"#respond\" onclick=\"suggestcomment_replace_comment('".$sugcomment->comment_id."')\">".stripslashes($sugcomment->comment_text)."</a></li>\n";
+}
+}
+echo "</ul>";
 }
 }
 
@@ -152,12 +252,24 @@ function suggestcomment_install () {
 		global $wpdb;
 		$table_name = $wpdb->prefix . "suggestcomment";
 
-   $suggestcomment_db_version = "0.2";
+   $suggestcomment_db_version = "0.8";
 	 $suggestcomment_describe = "Suggested comments<br><small>No own opinion? Choose one of mine ;)</small>";
-
+   $suggestcomment_prompt = "Replace current comment text?";
+	 $suggestcomment_posttypes_posts = "1";
+	 $suggestcomment_posttypes_pages = "1";
+	 $suggestcomment_randomize = "1";
+	 $suggestcomment_randomize_count = "3";
+	 
+	 
 	 add_option('suggestcomment_db_version', $suggestcomment_db_version,'Database version');
 	 add_option('suggestcomment_describe', $wpdb->escape($suggestcomment_describe),'Description');
-  
+ 	 add_option('suggestcomment_prompt', $wpdb->escape($suggestcomment_prompt),'Prompt');
+ 	 add_option('suggestcomment_posttypes_posts', $wpdb->escape($suggestcomment_posttypes_posts),'Show on Posts');
+ 	 add_option('suggestcomment_posttypes_pages', $wpdb->escape($suggestcomment_posttypes_pages),'Show on Pages');
+ 	 add_option('suggestcomment_randomize', $wpdb->escape($suggestcomment_randomize),'Show random comments');
+ 	 add_option('suggestcomment_randomize_count', $wpdb->escape($suggestcomment_randomize_count),'Random comments count');
+
+
 	
    if($wpdb->get_var("show tables like '$table_name'") != $table_name) {
     $sql = "CREATE TABLE " . $table_name . " (
@@ -179,7 +291,7 @@ function suggestcomment_install () {
 			}
      
   }
-   }
+ }
 
  //returns all comments	 
 function suggestcomment_get_comments(){
@@ -202,6 +314,14 @@ function suggestcomment_delete_comment($id){
  global $wpdb;
  $table_name = $wpdb->prefix . "suggestcomment";
  $sql = "DELETE FROM $table_name WHERE comment_id='$id'";
+ $results = $wpdb->query($sql);
+}
+
+//update comment
+function suggestcomment_update_comment($id,$text){
+ global $wpdb;
+ $table_name = $wpdb->prefix . "suggestcomment";
+ $sql = "UPDATE $table_name SET comment_text='".$wpdb->escape($text)."' WHERE comment_id='$id'";
  $results = $wpdb->query($sql);
 }
 
